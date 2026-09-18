e3d63c6bf5fee0332db5dbc54d698fc38b161151
Donghyun Kang / 강동현 <luckyandykang@gmail.com>
2026-09-18 14:04:49 +0900

    added merge sort tree initializer / 머지 소트 트리의 생성자 추가

--- data-structures/merge-sort-tree.cpp
+++ data-structures/merge-sort-tree.cpp
@@ -1,5 +1,17 @@
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 template<typename T> class MergeSortTree{
-	std::vector<int> tree;
+	std::vector<std::vector<T>> tree;
+    
+    MergeSortTree(vector<T>& vec){
+        int n = vec.size();
+        tree.resize(n << 1 | 1);
+        for (int i = 0; i < n; i++) tree[n + i].push_back(vec[i]);
+        for (int i = n - 1; i > 0; i--){
+            tree[i].resize(tree[i << 1].size() + tree[i << 1 | 1].size());
+            std::merge(tree[i << 1].begin(), tree[i << 1].end(), tree[i << 1 | 1].begin(), tree[i << 1 | 1].end(), tree[i].begin());
+        }
+    }
 };
